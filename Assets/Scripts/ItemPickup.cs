749f17e86bd09b5bf7f76d343fe1e098fb79c106
Artyom <a21plusartyom@gmail.com>
2021-05-14 13:33:38 +0300

    Animations

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+
+    public Item item;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        
+
+        Debug.Log("Picking up" + item.name);
+        //Add to inventory
+        Destroy(gameObject);
+    }
+
+}
